ed735a0d86f165d77c13b67a26e14102842dca3e
YangS <sarayang.song@gmail.com>
2018-09-28 10:17:18 -0700

    DP 9/28 started

---
+++ DP/LongestPalindromicSubstring.java
@@ -0,0 +1,38 @@
+package DP;
+
+/**
+ * Created by YANGSONG on 2018-09-28.
+ *
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+ Example 1:
+
+ Input: "babad"
+ Output: "bab"
+ Note: "aba" is also a valid answer.
+ Example 2:
+
+ Input: "cbbd"
+ Output: "bb"
+
+ Amazon
+ 12
+ Microsoft
+ 5
+ Adobe
+ 5
+ Google
+ 3
+ Baidu
+ 3
+ Alibaba
+ 3
+ Facebook
+ 2
+ Huawei
+ 2
+ IBM
+ 2
+ */
+public class LongestPalindromicSubstring {
+}
