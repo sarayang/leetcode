0863b601556670be69c1786956610889ef212f40
YangS <sarayang.song@gmail.com>
2018-10-10 02:01:00 -0700

    BB

---
+++ BB/LRU.java
@@ -0,0 +1,18 @@
+package Bloomberg;
+
+/**
+ * Created by YANGSONG on 2018-10-10.
+ */
+
+/**
+ * follow up question: what cache strategy you know
+ *
+ * cited "秒掉之后问我还知道哪些cache protocol，聊了聊FIFO, LFU。
+ * 然后问我如何修改上面写的LRU变成一个LFU，虽然对着问题灰常不开心，还是说了说LFU的设计方法。"
+ *
+ * Leetcode 460. LFU Cache / O(1) :
+ * kick off the least frequency item if no more capacity; for several items with same
+ * least frequency, use LRU rule (kick off the oldest one)
+ */
+public class LRU {
+}
